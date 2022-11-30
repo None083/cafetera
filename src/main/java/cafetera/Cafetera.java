e9046cd31faea1982964cf6fa929f38325477ddc
Noelia <perezgonzaleznoelia08@gmail.com>
2022-11-30 14:33:42 +0100

    creado

---
+++ src/main/java/cafetera/Cafetera.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cafetera;
+
+/**
+ *
+ * @author noelia
+ */
+public class Cafetera {
+    
+    double cantidadMaxima;
+    double cantidadActual;
+
+    public Cafetera() {
+    }
+
+    public Cafetera(double cantidadMaxima) {
+        this.cantidadActual = cantidadMaxima;
+    }
+
+    public Cafetera(double cantidadMaxima, double cantidadActual) {
+        this.cantidadMaxima = cantidadMaxima;
+        this.cantidadActual = cantidadActual;
+        if(cantidadActual > cantidadMaxima){
+            
+        }
+    }
+    
+    
+    
+    
+}
